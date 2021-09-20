e62350fbf5d1faf42005752821d6d8a7ebbce216
Pipo <prinsepipo.sanchez@gmail.com>
2021-09-20 21:28:33 +0800

    Integrate CMS Strapi
    Add strapi and create 'projects' and 'technologies' data.

---
+++ cms/README.md
@@ -0,0 +1,3 @@
+# Portfolio CMS (Strapi)
+
+This will handle the data source of the website.
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a89dbca9-4c5c-48fd-82f0-c0a82aea7b81'
+};
