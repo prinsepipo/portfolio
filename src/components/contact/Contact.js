5e60aca04bb6f8971bc2f7b9f6bed1a24d541296
Pipo <prinsepipo.sanchez@gmail.com>
2022-08-06 09:52:29 +0800

    Fix Gatsby warnings and errors

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -9,25 +9,27 @@ import ContentHeader from "../common/ContentHeader";
 import "./Contact.css";
 
 
-const handleSubmit = (e) => {};
-
-
 const Contact = () => {
   return (
     <ContentContainer id="contact" className="c-contact">
       <ContentHeader title="Get In Touch" />
       <p className="contact-comment">Got an opportunity? Want to collaborate? Send me a message.</p>
 
-      <ul class="contact-list">
+      <ul className="contact-list">
         <li className="contact-item contact-email">
           <a href="mailto:prinsepipo.sanchez@gmail.com" title="prinsepipo.sanchez@gmail.com">
-            <span class="contact-icon"><FontAwesomeIcon icon={faEnvelope} /></span>
+            <span className="contact-icon"><FontAwesomeIcon icon={faEnvelope} /></span>
             <span>prinsepipo.sanchez@gmail.com</span>
           </a>
         </li>
         <li className="contact-item contact-linkedin">
-          <a href="https://www.linkedin.com/in/josaphat-lumapas-350640180/" title="https://www.linkedin.com/in/josaphat-lumapas-350640180/" target="_blank">
-            <span class="contact-icon"><FontAwesomeIcon icon={faLinkedin} /></span>
+          <a
+            href="https://www.linkedin.com/in/josaphat-lumapas-350640180/"
+            title="https://www.linkedin.com/in/josaphat-lumapas-350640180/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="contact-icon"><FontAwesomeIcon icon={faLinkedin} /></span>
             <span>LinkedIn Profile</span>
           </a>
         </li>
