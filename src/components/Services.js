cae018d66021635fd96f853d7717f89b42ecd674
Pipo <prinsepipo.sanchez@gmail.com>
2022-09-05 20:44:41 +0800

    Update phrasing in mentoring service

--- src/components/Services.js
+++ src/components/Services.js
@@ -28,7 +28,7 @@ const Services = () => {
           <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-teacher-100-most-used-icons-flaticons-lineal-color-flat-icons.png" alt="Teacher Icon" />
           <div className="service-details">
             <h3>Mentoring</h3>
-            <p>I teach learners from basic programming and fundamentals to industry level web development skills.</p>
+            <p>I teach learners from basic programming fundamentals to industry level software development skills.</p>
             <a
               target="_blank"
               href="https://icons8.com/icon/khMkLuNVoAPH/teacher"
